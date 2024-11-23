da0530d76e65490529f66e38fac461b148d00c71
harshitkumark <153822358+harshitkumark@users.noreply.github.com>
2024-11-23 08:52:55 +0000

    container config done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,30 @@
+//let str1 = "hello"
+//let str2 = "hitesh"
+
+//let str3= str1 + str2
+
+//console.log(str3);
+
+//console.log("1" +2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+
+/*let gamecounter =100
+ ++gamecounter ;
+ console.log(gamecounter);
+ */
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >=0); 
+
+
+console.log(undefined == 0);
+console.log(undefined >0);
+console.log(undefined < 0);
+
+console.log("2"===2);
