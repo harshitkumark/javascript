28146f6efdd86fae188980ccf6a4bfa329ccb185
harshitkumark <153822358+harshitkumark@users.noreply.github.com>
2024-11-23 09:12:45 +0000

    container config done

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,14 @@
+ const score =100
+
+ const scorevalue=100.3
+
+
+ const isLoggedIN = false
+ const outsideTemp=null
+
+  let userEmail;
+
+  const id = Symbol('123')
+  const anotherId=Symbol('123')
+
+  console.log(id == anotherId);
