198c02ae8561472c31db47c68451be20c577a269
harshitkumark <153822358+harshitkumark@users.noreply.github.com>
2024-11-26 06:48:09 +0000

    container confif done

---
+++ 01_basics/stackandheap.js
@@ -0,0 +1,25 @@
+// stack (Primmitive) , heap(Non primitive)
+
+let myyoutubename = "hiteshchoudharydotcom"
+
+let anothername = myyoutubename
+anothername = "chaiaurcode"
+
+
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userOne ={
+    email: "user@google.com",
+    upi :"user@ybl"
+
+}
+let userTwo = userOne
+
+userTwo.email = "harshit@google.com"
+
+console.log(userOne.email);
+
+console.log(userTwo.email);
+
---
+++ 01_basics/string.js
@@ -0,0 +1,8 @@
+const name = "harshit"
+const repocount =6
+
+//console.log(name +repocount +"value");
+// we have another method also to write these
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
