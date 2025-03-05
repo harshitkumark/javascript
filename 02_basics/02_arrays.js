d767f3fb97161fc53a6be846e20c234130ac1dc8
harshitkumark <153822358+harshitkumark@users.noreply.github.com>
2025-03-05 21:00:00 +0000

    container config done

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -16,3 +16,18 @@ myArr.pop(7)//pop is for remove somethings
 
 
 console.log(myArr);
+
+
+// slice , splice 
+
+console.log("A" ,myArr);
+
+const myn1= myArr.slice(1,3)
+
+console .log (myn1);
+console .log("B ",myArr);
+
+const myn2= myArr.splice(1,3)
+console.log("C ", myArr)
+ console .log (myn2);
+
