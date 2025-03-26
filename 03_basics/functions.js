84444622e19cda18fbe8c116451e1e9607ca5cdc
harshitkumark <153822358+harshitkumark@users.noreply.github.com>
2025-03-26 18:29:30 +0000

    cont config done

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -8,3 +8,28 @@ function sayMyName (){
 
 //sayMyName()
 
+function loginUserMessage(username){
+    if (!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username}just logged in`
+
+}
+
+//console.log(loginUserMessage("ANSHU"))
+
+//console.log(loginUserMessage())
+
+//like an  ekart the price adding system 
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${
+        anyobject.price}`)
+
+}
+//handleobject(user)
+handleobject({
+    username : "harshit",
+    price :399
+})
