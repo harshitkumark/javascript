e809c3cd75349f70bf719a456c842efa33800c45
harshitkumark <153822358+harshitkumark@users.noreply.github.com>
2025-03-27 13:30:00 +0000

    cont config done

---
+++ 05_iterations/one.js
@@ -0,0 +1,19 @@
+// for 
+ for (let index = 0; index <=10; index++) {
+    const element = index;
+   // console.log(element);
+
+    
+ }
+
+
+ //Multiply code 
+
+for (let i = 1; i<=10; i++) {
+   console .log (`Outer loop value:${i}`);
+   for (let j = 1; j<=10; j++){
+      console.log(` ${j} and  ${i}`);
+      console .log(i+'*'+j+'='+i*j);
+
+   }
+}
